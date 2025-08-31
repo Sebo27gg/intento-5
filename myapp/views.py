141f5deb37ca50df2ed1d5df044f93c56e3b6d14
sebo <dasd@gmail.com>
2025-08-31 16:31:48 -0400

    cosito

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def myapp(request):
+    return HttpResponse("")
+def home(request):
+    return HttpResponse("<h1>oliwi<h1>")
